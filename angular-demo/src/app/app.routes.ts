23ee562372edb4485e00957cb252cbf902608c19
Apuroksiczek <82889581+Apuroksiczek@users.noreply.github.com>
2025-02-24 22:41:38 +0100

    abc

--- angular-demo/src/app/app.routes.ts
+++ angular-demo/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Test1Component } from './ui/test1/test1.component';
+import { Test2Component } from './ui/test2/test2.component';
 
-export const routes: Routes = [];
+const routeConfig: Routes = [
+    {
+        path: 'test1',
+        component: Test1Component,
+        title: 'Home page',
+    },
+    {
+        path: 'test2',
+        component: Test2Component,
+        title: 'Home details',
+    },
+];
+export default routeConfig;
