118be7f719f661a0796cafdce844ca1affee344f
Apuroksiczek <82889581+Apuroksiczek@users.noreply.github.com>
2025-02-27 19:52:21 +0100

    asdgads

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
+import { map, Observable } from 'rxjs';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-demo';
+
+  private readonly http = inject(HttpClient);
+
+  image$: any;
+  isButtonClicked = false;
+
+  getDog() {
+    this.image$ = this.http.get('https://dog.ceo/api/breeds/image/random').pipe(map((x: any) => x.message))
+    this.isButtonClicked = true
+  }
+
 }
